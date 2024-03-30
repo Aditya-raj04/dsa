ac58ab92d95cbb13b4dd607a340ade9bcc6cc3ce
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-03-30 20:57:21 +0000

    java intro

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// class HelloWorldJava{
+// public static void main (String[]args){
+//     System.out.print("aditya raj");
+// }
+// }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
