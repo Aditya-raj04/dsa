a0dfa1ec4c5604c19b80448ea760c4da5b289fc3
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-03-31 14:07:14 +0000

    java intro

--- java/main.java
+++ java/main.java
@@ -3,12 +3,34 @@
 //         System.out.println("Hello, world!");
 //     }
 // }
-public class main{
+// public class main{
+//     public static void main(String[] args) {
+//     System.out.println("hello");
+//     }
+// }
+
+
+public class main {
+    private String name;
+    private int age;
+
+    // Constructor
+    public main(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method definition
+    public void introduce() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+
     public static void main(String[] args) {
-    System.out.println("hello");
+        // Creating object of Person class
+        main person1 = new main("John", 25);
+
+        // Calling method on object
+        person1.introduce();
     }
 }
 
-public class main{
-    
-}
---
+++ java/news.java
@@ -0,0 +1,19 @@
+ class news{
+    public static void main(String[] args){
+        System.out.println("hello");
+        System.out.println(2+5);
+        int num = 2;
+        System.out.println(num);
+
+        byte a = 127;
+        short sh = 558;
+        long l = 1234l;
+
+        float f = 45.8f;
+        double d = 234.897;
+
+        char c = 'k';
+        boolean b = true;
+        System.out.println(b);
+    }
+}
