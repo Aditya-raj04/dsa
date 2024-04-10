04eb9e32b87e7c237a294b0017d4de44be49b42b
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-04-10 17:05:09 +0000

    java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,21 @@
 //     System.out.print("aditya raj");
 // }
 // }
+
+
+// literals
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+    int num1 = 0b101;
+    System.out.println(num1);
+
+    int num = 1_00_00_000;
+    System.out.println(num);
+
+    char c = 'c';
+    c++;
+    System.out.println(c);
     }
 }
---
+++ java/hell.java
@@ -0,0 +1,22 @@
+class hell{
+    public static void main(String args[]){
+        byte b = 127;
+        System.out.println(b);
+
+        int a  = 257;
+        byte c =(byte) a;           //type casting
+        System.out.println(c);
+
+        float f = 24.9f;
+        int d  =(int)f;
+        System.out.println(d);
+
+        byte x = 10;
+        byte y = 30;
+        int result = x*y;
+        System.out.println(result);
+
+        
+
+    }
+}
