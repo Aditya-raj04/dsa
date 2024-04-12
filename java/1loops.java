ffd314755d3ffec1428eafcbf98d8929d5a97336
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-04-12 18:08:03 +0000

    java

---
+++ java/1loops.java
@@ -0,0 +1,67 @@
+class loops{ 
+    public static void main(String args[]){
+      int a = 1;
+
+      // while (a<5){
+      // System.out.println("hello" + a);
+      // int i = 1;
+      //     while(i <= 2){
+      //       System.out.println("aditya" + i);
+      //       i++;
+      //     }
+      // a++;
+    // }
+
+
+      // do{
+      //    System.out.println("hello"+a);
+      //    a++;
+      // }while(a<5);
+
+
+      for(byte i = 1 ;i< 8;i++){
+        System.out.println("day "+i);
+        for(int j = 1;j<9;j++){
+          System.out.println("    " + ( j+8 ) + " _ " + (j+9));
+        }
+      }
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      
+    }
+}
