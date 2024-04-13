5489eb9369f9087e1001fb96f27909e97d376eda
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-04-13 19:56:15 +0000

    java

--- java/classes.java
+++ java/classes.java
@@ -1,5 +1,22 @@
-class classes{
-    public static void main(String args[]){
-System.out.println("hello");
+class calculator{
+    int a;
+    public int add(){
+        System.out.println("in add");
+        return 0;
     }
 }
+public class classes{
+    public static void main(String a[]){
+System.out.println("hello");
+calculator calc = new calculator();
+calc.add();
+    }
+}
+
+
+
+
+
+
+
+
