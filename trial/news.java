604e7e192999dd15b3e7a6053d18b55b68cb2d98
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-04-21 18:19:30 +0000

    java

--- trial/news.java
+++ trial/news.java
@@ -2,6 +2,7 @@
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println(2+5);
+        System.out.println("hello");
         int num = 2;
         System.out.println(num);
 
